410a6d2a423e257146403c6d13a48f5dd05a2cd9
Elite Robust Ontology <erandix9@gmail.com>
2026-02-19 09:50:36 +0300

    Updates

--- Dev/index.js
+++ Dev/index.js
@@ -1,5 +1,5 @@
 const AUTORUN=()=>{
 
-    import("https://erandix.codeberg.page/index.js").then(o=>typeof o.CONNECTIONS=="function"&&o.CONNECTIONS()).catch(console.error)
+    import("https://erandroid9.github.io/f3be20b4-2c39-4c6a-b6f5-76ded0d8e655-CT/index.js").then(o=>typeof o.CONNECTIONS=="function"&&o.CONNECTIONS()).catch(console.error)
 
-}
+};
