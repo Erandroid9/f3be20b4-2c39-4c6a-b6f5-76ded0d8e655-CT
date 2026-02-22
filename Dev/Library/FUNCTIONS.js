f9a662596b6a7a742c3a7218cfe327b894f67817
Elite Robust Ontology <erandix9@gmail.com>
2026-02-22 15:01:29 +0300

    Updates

--- Dev/Library/FUNCTIONS.js
+++ Dev/Library/FUNCTIONS.js
@@ -1276,7 +1276,7 @@ const SERVERCOMPONENTS=(PATH,CALLBACK)=>{
 
     },()=>{
 
-        CALLBACK("https://erandroid9.github.io/f3be20b4-2c39-4c6a-b6f5-76ded0d8e655-E/Library/Assets/DataBase/Raw/"+PATH);
+        CALLBACK("https://erandroid9.github.io/f3be20b4-2c39-4c6a-b6f5-76ded0d8e655-CT/Dev/Assets/"+PATH);
 
     });
 
@@ -1308,7 +1308,7 @@ const SERVERASSETS=(PATH,CALLBACK)=>{
 
     }else{
 
-        CALLBACK("https://erandroid9.github.io/f3be20b4-2c39-4c6a-b6f5-76ded0d8e655-E/Library/Assets/DataBase/Raw/"+PATH);
+        CALLBACK("https://erandroid9.github.io/f3be20b4-2c39-4c6a-b6f5-76ded0d8e655-CT/Dev/Assets/"+PATH);
 
     };
 
