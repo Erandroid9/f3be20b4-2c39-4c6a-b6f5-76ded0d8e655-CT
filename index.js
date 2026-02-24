99f032c430191d488f48795d0d03e9b32a38f605
Elite Robust Ontology <erandix9@gmail.com>
2026-02-24 08:43:02 +0300

    Updates

--- index.js
+++ index.js
@@ -481,7 +481,7 @@ const PROJECTFETCH = (DIRECTLINK, DATABASE) => {
 };
 
 const STYLESDOWNLOAD=()=>{
-    fetch("https://erandroid9.github.io/f3be20b4-2c39-4c6a-b6f5-76ded0d8e655-CT/Dev/index.css/")
+    fetch("https://erandroid9.github.io/f3be20b4-2c39-4c6a-b6f5-76ded0d8e655-CT/Dev/index.css")
     .then(res =>res.text())
     .then(data =>{
         localStorage.setItem("APPCSS",data);
