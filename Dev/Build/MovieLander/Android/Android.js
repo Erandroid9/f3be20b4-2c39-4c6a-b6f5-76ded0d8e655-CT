798bb690106e7e4d2d33809ca7ece6c9fddaab5b
f3be20b4-2c39-4c6a-b6f5-76ded0d8e655 <erandix9@gmail.com>
2026-02-27 12:02:11 +0300

    Updates

---
+++ Dev/Build/MovieLander/Android/Android.js
@@ -0,0 +1,58 @@
+const ERANDES=()=>{
+
+    VIEWCONTROLLER();
+
+};
+
+const MOBILEVIEW=()=>{
+
+    HEADERVIEW((ELEMENT)=>{
+
+        TEXTVIEW(ELEMENT,"Data",(ES)=>{
+
+            CLICK(ES,()=>{
+
+                REFRESH();
+
+            });
+
+        });
+
+    },(ELEMENT)=>{
+
+        TEXTVIEW(ELEMENT,sessionStorage.getItem("AndroidSms"),()=>{
+
+        });
+
+        BREAK(ELEMENT);
+
+        BUTTON(ELEMENT,"Check Balance 1",FORESTGREEN,(ELs)=>{
+
+            CLICK(ELs,()=>{
+
+                runUssd("*131#","1");
+
+            });
+
+        });
+
+        BREAK(ELEMENT);
+
+        BUTTON(ELEMENT,"Check Balance 2",FORESTGREEN,(ELs)=>{
+
+            CLICK(ELs,()=>{
+
+                runUssd("*131#","2");
+
+            });
+
+        });
+
+    });
+
+};
+
+const DESKTOPVIEW=()=>{
+
+
+};
