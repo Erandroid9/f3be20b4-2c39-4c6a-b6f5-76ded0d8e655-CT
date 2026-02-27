4ce8799a1bd25405bd50e79d7a51ccb849a76e53
f3be20b4-2c39-4c6a-b6f5-76ded0d8e655 <erandix9@gmail.com>
2026-02-27 18:50:26 +0300

    Updates

--- Dev/Build/MovieLander/Android/Android.js
+++ Dev/Build/MovieLander/Android/Android.js
@@ -8,10 +8,41 @@ const MOBILEVIEW=()=>{
 
     HEADERVIEW((ELEMENT)=>{
 
+        TEXTVIEW(ELEMENT,"Data",(ES)=>{
+
+            CLICK(ES,()=>{
+
+                REFRESH();
+
+            });
+
+        });
 
     },(ELEMENT)=>{
 
-        
+        BREAK(ELEMENT);
+
+        BUTTON(ELEMENT,"Check Balance 1",FORESTGREEN,(ELs)=>{
+
+            CLICK(ELs,()=>{
+
+                runUssd("*131#","1");
+
+            });
+
+        });
+
+        BREAK(ELEMENT);
+
+        BUTTON(ELEMENT,"Check Balance 2",FORESTGREEN,(ELs)=>{
+
+            CLICK(ELs,()=>{
+
+                runUssd("*131#","2");
+
+            });
+
+        });
 
     });
 
